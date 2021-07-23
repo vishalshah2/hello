5bd1fb43fca8d0296cf500f5cca47b18dc3a812c
vishalshah2 <87510734+vishalshah2@users.noreply.github.com>
2021-07-23 10:55:45 -0600

    Create indent.py

---
+++ indent.py
@@ -0,0 +1,17 @@
+import time
+indent = 0
+indentIncreasing = True
+
+while True:
+    print(' ' * indent, end='')
+    print('*****')
+    time.sleep(0.1)
+    if indentIncreasing:
+        indent = indent + 1
+        if indent == 20:
+            indentIncreasing = False
+        
+    else:
+        indent = indent -1
+        if indent == 0:
+            indentIncreasing = True
