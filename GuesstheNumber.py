29504830f07a078bd3cbc82a4a3155f6bf79ee12
vishalshah2 <87510734+vishalshah2@users.noreply.github.com>
2021-08-05 11:18:25 -0600

    Update GuesstheNumber.py

--- GuesstheNumber.py
+++ GuesstheNumber.py
@@ -1,15 +1,30 @@
+import random, sys
 
-import sys
-import random
-randomnumber = random.randint(1, 20)
-print('I am thinking of a number between 1 and 20.')
-print('Take a guess.')
+def guess(x):
+    random_number = random.randint(1 , x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a Number between 1 and {x}: '))
+        if guess < random_number:
+            print('Too low')
+        elif guess > random_number:
+            print('Too high')
 
-for i in range(1,7):
-    guesstaken = int(input())
-    if guesstaken > randomnumber:
-        print('Your guess is too high.')
-    elif guesstaken < randomnumber:
-        print('Your guess is too low.')
-    else:
-        print('Good job! You guessed my number ' + str(i) + ' guesses!')
+    print('you have guess the number')
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c' and low!= high:
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (c)??').lower()
+        if feedback == 'h':
+            high = guess -1
+        if feedback == 'l':
+            low = guess + 1
+
+    print('computer have guess the number')
+        
+            
+computer_guess(10)  
